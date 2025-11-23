40914ba0e1339366d6611ef29215ddc07d59018c
Gu <1137801566@qq.com>
2025-11-23 15:45:36 +0800

    初始项目结构

---
+++ 未命名1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	printf("guxin shige shabi");
+	
+	
+	 return 0;
+} 
+
+
+
+
---
+++ 未命名8.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	scanf("请输入一个正三位数”，abc, %d\n");
+	printf("输出逆序的数cba,%d\n");
+	return 0;
+}
